3a32ebc54dadaa0b11ce66901dc0e38deeaa896e
Wind_shaow <FX>
2024-04-06 22:05:56 +0800

    fix

--- oj_server/oj_server.cpp
+++ oj_server/oj_server.cpp
@@ -1,13 +1,15 @@
-//oj_server 采用 M C V 模式
-
-#include"../comm/httplib.h"
-#include"oj_control.hpp"
-#include<iostream>
+// oj_server 采用 M C V 模式
+//  #define CPPHTTPLIB_OPENSSL_SUPPORT //https
+#include "../comm/httplib.h"
+#include "oj_control.hpp"
+#include <iostream>
+#include <openssl/ssl.h>
+#include <openssl/crypto.h>
+#include <openssl/opensslv.h>
 using namespace std;
 using namespace httplib;
 
-
-int port=8080;
+int port = 8080;
 
 static Control *ctrl_ptr = nullptr;
 
@@ -19,56 +21,49 @@ void Recovery(int signo)
 int main()
 {
     signal(SIGQUIT, Recovery);
-
-    //用户请求的服务路由功能
-    Server srv;
+    // 用户请求的服务路由功能
+    httplib::Server srv;
 
     Control ctrl;
     ctrl_ptr = &ctrl;
 
-
-    //请求题库
-    srv.Get("/all_questions",[&ctrl](const Request&reqs,Response&reps)
-    {
+    // 请求题库
+    srv.Get("/all_questions", [&ctrl](const Request &reqs, Response &reps)
+            {
         string html;
 
         ctrl.All_questsionlist(html);
-        reps.set_content(html,"text/html;charset=utf-8");
-    });
+        reps.set_content(html,"text/html;charset=utf-8"); });
 
-    //请求题目
-    srv.Get(R"(/question/(\d+))",[&ctrl](const Request&reqs,Response&reps)
-    {
-        string number=reqs.matches[1];
-        string html;
-        
-        ctrl.One_questsion(number,html);
-        reps.set_content(html,"text/html;charset=utf-8");
-        
-    });
+    // 请求题目
+    srv.Get(R"(/question/(\d+))", [&ctrl](const Request &reqs, Response &reps)
+            {
+                string number = reqs.matches[1];
+                string html;
 
-    //对题目进行判题
-    srv.Post(R"(/judge/(\d+))",[&ctrl](const Request&reqs,Response&reps)
-    {
+                ctrl.One_questsion(number, html);
+                reps.set_content(html, "text/html;charset=utf-8");
+            });
+
+    // 对题目进行判题
+    srv.Post(R"(/judge/(\d+))", [&ctrl](const Request &reqs, Response &reps)
+             {
         string number=reqs.matches[1];
         string res_json;
         ctrl.Judge(number,reqs.body,res_json);
         
-        reps.set_content(res_json,"application/json;charset=utf-8");
-    });
+        reps.set_content(res_json,"application/json;charset=utf-8"); });
 
-
-    //对题目进行测试运行 不含判题
-    srv.Post(R"(/Testrun/(\d+))",[&ctrl](const Request&reqs,Response&reps)
-    {
+    // 对题目进行测试运行 不含判题
+    srv.Post(R"(/Testrun/(\d+))", [&ctrl](const Request &reqs, Response &reps)
+             {
         string number=reqs.matches[1];
         string res_json;
         ctrl.Testrun(number,reqs.body,res_json);
 
-        reps.set_content(res_json,"application/json;charset=utf-8");
-    });
+        reps.set_content(res_json,"application/json;charset=utf-8"); });
 
     srv.set_base_dir("./wwwroot/");
-    srv.listen("0.0.0.0",port);
+    srv.listen("0.0.0.0", port);
     return 0;
 }
