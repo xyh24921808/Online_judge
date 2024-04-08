a44b1321e7dbe22688178d95a4d5ea6056075cb1
Wind_shaow <FX>
2024-04-08 21:18:18 +0800

    quest_list

---
+++ oj_server/questions/2/header.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+class Solution
+{
+public:
+    int NumMax(vector<int>&nums)
+    {
+        
+        return 0;
+    }
+};
---
+++ oj_server/questions/2/tail_main.cpp
@@ -0,0 +1,13 @@
+#ifndef COMPILER_ONLINE
+
+#include "header.cpp"
+
+#endif
+
+
+int main(void)
+{
+    vector<int>vc;
+    Solution ().NumMax(vc);
+    return 0;
+}
